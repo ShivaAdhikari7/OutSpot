f0f677ded7ee713c3e25285c0ad24ec3dff13371
Shiva Adhikari <79002274+ShivaAdhikari7@users.noreply.github.com>
2023-04-24 16:50:55 +0545

    complete blog and spot section

--- outspot_frontend/src/pages/HomePage.js
+++ outspot_frontend/src/pages/HomePage.js
@@ -1,9 +1,12 @@
 import Navbar from "../components/ui/Navbar/Navbar";
+import BlogSection from "./BlogPage/BlogSection";
+import SpotSection from "./SpotPage/SpotSection";
 const HomePage = () => {
   return (
     <>
       <Navbar />
-      <p>Home Page</p>
+      <BlogSection />
+      <SpotSection />
     </>
   );
 };
